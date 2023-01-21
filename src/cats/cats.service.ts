4817e80a4bc5e8365b2061d42c4b265bcc9fe138
BVBFD <lsevina126@gmail.com>
2023-01-21 23:10:54 +0900

    add repository layer logic

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -8,14 +8,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Cat, CatDocument } from './cats.schema';
 import { CatRequestDto } from 'src/dto/cats.request.dto';
 import * as bcrypt from 'bcrypt';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existsByEmail(email);
 
     if (isCatExist) {
       // throw new HttpException('해당하는 고양이는 이미 존재합니다.', 403);
@@ -25,7 +26,8 @@ export class CatsService {
     const saltOrRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltOrRounds);
 
-    const cat = await this.catModel.create({
+    const cat = await this.catsRepository.create({
+      ...body,
       email,
       name,
       password: hashedPassword,
--- src/dto/cats.request.dto.ts
+++ src/dto/cats.request.dto.ts
@@ -1,5 +1,3 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 import { Cat } from 'src/cats/cats.schema';
 
 export class CatRequestDto extends Cat {}
